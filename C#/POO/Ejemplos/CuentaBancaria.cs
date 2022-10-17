12ab4a3377a20139b9aafdb19f52ba26e8d5691a
Gerardo Beltrán <contacto@gerardobeltran.mx>
2022-10-17 17:34:28 +0000

    --

--- C#/POO/Ejemplos/CuentaBancaria.cs
+++ C#/POO/Ejemplos/CuentaBancaria.cs
@@ -2,11 +2,20 @@ using System;
 class HelloWorld {
   static void Main() {
     CuentaBancaria MiCuenta = new CuentaBancaria( );
+    CuentaBancaria OtraCuenta = new CuentaBancaria();
+    MiCuenta.CambiarNombre("Luis Lopez");
     MiCuenta.Deposito(2000);
     MiCuenta.Retiro(500);
+    
+    OtraCuenta.Deposito(1234);
     decimal Saldo ;
     Saldo = MiCuenta.ObtenerSaldo();
-    Console.WriteLine("El saldo del cliente es {0:C}", Saldo);
+    Console.WriteLine("El saldo del cliente {0} es {1:C}", 
+                      MiCuenta.ObtenerNombre(),
+                      Saldo);
+     Console.WriteLine("El saldo de otro cliente es {0:C}",       
+                OtraCuenta.ObtenerSaldo());
+ 
   }
 }
 
@@ -14,6 +23,12 @@ class CuentaBancaria
 {
   private decimal saldo;
   private string nombre;
+  public string ObtenerNombre(){
+    return nombre;
+  }
+  public void CambiarNombre(string nombre){
+    this.nombre = nombre;
+  }
   public void Retiro(decimal cantidad){
     saldo-=cantidad;
   }
