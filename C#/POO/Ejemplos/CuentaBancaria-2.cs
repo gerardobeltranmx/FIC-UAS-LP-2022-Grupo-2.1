515382500b0eb109f183dcbcde3e5dcf33c46825
Gerardo Beltrán <contacto@gerardobeltran.mx>
2022-10-18 17:27:10 +0000

    --

---
+++ C#/POO/Ejemplos/CuentaBancaria-2.cs
@@ -0,0 +1,69 @@
+using System;
+class Programa {
+  static void Main() {
+
+    CuentaBancaria MiCuenta = new CuentaBancaria("Luis Perez", 50);
+    
+    Console.WriteLine(MiCuenta.ObtenerNombre());
+    Console.WriteLine(MiCuenta.ObtenerSaldo());
+    MiCuenta.Deposito(1000);
+    Console.WriteLine(MiCuenta.ObtenerSaldo());
+    
+    
+    
+    
+   /* CuentaBancaria MiCuenta2 = new CuentaBancaria();
+    MiCuenta.EstableceNombre("Juan");
+    MiCuenta.Deposito(-1000);
+    Console.WriteLine(MiCuenta.ObtenerSaldo());
+    MiCuenta.Retiro(1300);
+    Console.WriteLine(MiCuenta.ObtenerNombre());
+    Console.WriteLine(MiCuenta.ObtenerSaldo());
+    
+    MiCuenta2.Deposito(1234);
+    Console.WriteLine(MiCuenta2.ObtenerSaldo());
+    
+    */
+  }
+}
+
+class CuentaBancaria
+{
+  private decimal saldo;
+  private string nombre;
+  
+  public CuentaBancaria(){
+      saldo = 0;
+      nombre = "Desconocido";
+  }
+  
+  public CuentaBancaria(string nombre, decimal saldo){
+      this.nombre = nombre;
+      this.saldo = saldo;
+
+  }
+  
+  
+  
+  public void Retiro(decimal cantidad){
+    if (saldo >= cantidad)  
+      saldo-=cantidad;
+    else
+      Console.WriteLine("Saldo insuficiente: {0:C}", saldo);
+  }
+  public void Deposito(decimal cantidad){
+    if (saldo > 0 )
+      saldo+=cantidad;
+    else 
+      Console.WriteLine("Error: cantidad debe ser superior a 0 (cero); ");
+  }
+  public decimal ObtenerSaldo( ){
+    return saldo;
+  }
+  public void EstableceNombre(string nombre){
+    this.nombre = nombre;
+  }
+  public string ObtenerNombre(){
+    return this.nombre;
+  }
+}
--- C#/POO/Ejercicios/A. Area del circulo.cs
+++ C#/POO/Ejercicios/A. Area del circulo.cs
@@ -2,18 +2,14 @@ using System;
 class Programa {
   static void Main() {
     Circulo Obj = new Circulo();
-
     double Radio = double.Parse(Console.ReadLine());  
-    
     Obj.CambiarRadio(Radio);
-    
     Console.WriteLine("{0:F2}", Obj.ObtenerArea() );
   }
 }
 
 class Circulo {
   private double Radio;
-
   public void CambiarRadio(double Radio){
      this.Radio = Radio;
   }
